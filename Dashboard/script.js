7b5de9dd891da94e91922176ff4f5ef543e043b6
Barbara Haggi <barbarahaggi3@gmail.com>
2024-09-18 17:40:58 -0300

    adiçao das abas transacoes e dashboard

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -1,47 +1,65 @@
+const ctx = document.getElementById('grafico-meses');
 
-        // Dados para o gráfico de receitas
-        const receitasCtx = document.getElementById('receitasChart').getContext('2d');
-        new Chart(receitasCtx, {
-            type: 'bar',
-            data: {
-                labels: ['Último Mês'],
-                datasets: [{
-                    label: 'Receita',
-                    data: [24], // Aqui está o valor da receita em porcentagem
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto'],
+      datasets: [{
+        label: 'Receita dos meses anteriores',
+        data: [120, 190, 130, 150, 120, 130, 100, 111],
+        borderWidth: 1,
+        backgroundColor: '#FFEBCD',
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  })
 
-        // Dados para o gráfico de despesas
-        const despesasCtx = document.getElementById('despesasChart').getContext('2d');
-        new Chart(despesasCtx, {
-            type: 'bar',
-            data: {
-                labels: ['Último Mês'],
-                datasets: [{
-                    label: 'Despesa',
-                    data: [5], // Aqui está o valor da despesa em porcentagem
-                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                    borderColor: 'rgba(255, 99, 132, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-  
+  const grafico1 = document.getElementById('grafico1');
+
+  new Chart(grafico1, {
+    type: 'bar',
+    data: {
+      labels: ['Setembro'],
+      datasets: [{
+        label: 'Receita',
+        data: [116],
+        borderWidth: 1,
+        backgroundColor: '#006400',
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+})
+
+const grafico2 = document.getElementById('grafico2');
+
+  new Chart(grafico2, {
+    type: 'bar',
+    data: {
+      labels: ['Setembro'],
+      datasets: [{
+        label: 'Despesa',
+        data: [116],
+        borderWidth: 1,
+        backgroundColor: '#FF0000',
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+})
